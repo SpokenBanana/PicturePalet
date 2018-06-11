e0162bde93cf24891bea5c007eb85fd734f8eb67
David Santana <santanavidda@gmail.com>
2018-06-11 18:21:39 -0400

    Ported to Python3.

--- bot.py
+++ bot.py
@@ -2,58 +2,63 @@ import twitter
 from PIL import Image
 import urllib
 import io
+import os
 from picture_pallet import Palette
 
-api = twitter.Api(consumer_key='',
-                  consumer_secret='',
-                  access_token_key='',
-                  access_token_secret='')
+api = twitter.Api(consumer_key=os.environ['TWITTER_KEY'],
+                  consumer_secret=os.environ['TWITTER_SECRET'],
+                  access_token_key=os.environ['TWITTER_TOKEN'],
+                  access_token_secret=os.environ['TWITTER_TOKEN_SECRET'])
 
 
 def get_last():
+    if not os.path.isfile('last.txt'):
+        return None
     with open('last.txt', 'r+') as last_file:
         last = last_file.readline().strip()
     return last
 
 
 def get_image(mention_media):
-    fd = urllib.urlopen(mention_media['media_url_https'])
+    fd = urllib.request.urlopen(mention_media['media_url_https'])
     image_file = io.BytesIO(fd.read())
     return Image.open(image_file).convert('RGB')
 
 
 def reply_to(mention):
     if len(mention['media']) < 2:
-        api.PostUpdate('Sorry @%s! I need two pictures!',
-                       in_reply_to_status_id=mention['id'])
+        api.PostUpdate('Sorry @{}, I need two pictures!'.format(
+            mention['user']['screen_name']),
+            in_reply_to_status_id=mention['id'])
         return
 
     im = get_image(mention['media'][0])
     im2 = get_image(mention['media'][1])
 
-    palette = Palette(im, im2).generate_picture()
+    palette = Palette(im, im2).generate_picture(
+        file_name='{}.jpg'.format(mention['id']))
 
-    api.PostMedia('@%s' % mention['user']['screen_name'], palette,
-                  in_reply_to_status_id=mention['id'])
+    api.PostUpdate('@{}'.format(mention['user']['screen_name']),
+                   in_reply_to_status_id=mention['id'], media=palette)
 
 
 def start():
     last = get_last()
 
-    mentions = api.GetMentions(1, since_id=last)
+    mentions = api.GetMentions(since_id=last)
 
-    print "{0:d} mentions.".format(len(mentions))
+    print("{0:d} mentions.".format(len(mentions)))
 
     for mention in mentions:
         as_dict = mention.AsDict()
-        print 'replying to %s' % as_dict['user']['screen_name']
+        print('replying to {}'.format(as_dict['user']['screen_name']))
         reply_to(as_dict)
+        print('replied to {}'.format(as_dict['user']['screen_name']))
 
-        # keep track of the last mention replied to
+    # keep track of the last mention replied to
+    if len(mentions):
         with open('last.txt', 'w') as last_file:
-            last_file.write(str(as_dict['id']))
-
-        print 'replied to %s' % as_dict['user']['screen_name']
+            last_file.write(str(mentions[0].id))
 
 
 if __name__ == '__main__':
